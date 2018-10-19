0796581ad60a6702db12b8bded807852addb44ed
Yan Vaidakovich <wox.chentsu@gmail.com>
2018-10-19 18:54:09 +0300

    Написать таймер обратного отсчета

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let timerBox  = document.body.getElementsByClassName('timerBox')[0];
+  let deaLine   = new Date(2018,9,20),
+      timer = deaLine - Date.now();
+  
+  // console.log(timer.valueOf());
+  if (timer > 0) {
+    let timerId = setInterval(() => {
+        let tmp = [(timer/1000/60/60).toFixed(0), (timer/1000/60%60).toFixed(0), (timer/1000%60).toFixed(0)];
+
+        for (let i=0; i<3; i++){
+          if (tmp[i].length === 1){
+            tmp[i] = '0' + tmp[i];
+          }
+        }
+        timerBox.textContent = tmp.join(':');
+      timer -= 1000;
+    }, 1000);
+  } else {
+    timerBox.textContent = '00:00:00';
+  }
+  
+});
