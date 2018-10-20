51356c4df44a792ce10ff8f963fb25640e7c666b
Yan Vaidakovich <wox.chentsu@gmail.com>
2018-10-20 20:57:43 +0300

    Написать скрипт плавной прокрутки страницы при клике на элементы меню, используя чистый JS

---
+++ js/scroll.js
@@ -0,0 +1,48 @@
+'use strict';
+
+let navMenu   = document.body.getElementsByTagName('nav')[0],
+    navLinks  = navMenu.getElementsByTagName('a');
+
+// порядок секций в массиве должен соответствовать порядку элементов меню
+let sections = [
+  document.getElementById('about'),
+  document.getElementById('photo'),
+  document.getElementById('price'),
+  document.getElementById('contacts')
+];
+
+
+navMenu.addEventListener('click', evt => { // ткнули в блок меню
+  if (evt.target.tagName === 'A'){ // ещё и в тег А
+    evt.preventDefault(); // блокируем стандартное поведение ткнутого элемента
+    for (let i=0; i<navLinks.length; i++){ // перебираем все элементы чтоб выяснить номер ткнутого
+      if (evt.target === navLinks[i]){
+        scrollToElement(sections[i]); // крутим страницу к секции с тем же номером что у ткнутого элемента
+      }
+    }
+  }
+});
+
+
+function scrollToElement(nodeElement) {
+  let scrollDistance  = document.documentElement.scrollTop + nodeElement.getBoundingClientRect().top - navMenu.clientHeight;
+  let animationID,
+      step = 10;
+  
+  shift();
+  
+  function shift() {
+  
+    animationID = requestAnimationFrame(shift);
+  
+    if (document.documentElement.scrollTop === scrollDistance){
+      cancelAnimationFrame(animationID);
+    }
+    
+    if (document.documentElement.scrollTop < scrollDistance){
+      scrollTo(0, document.documentElement.scrollTop + step);
+    }else if (document.documentElement.scrollTop > scrollDistance){
+      scrollTo(0, document.documentElement.scrollTop - step);
+    }
+  }
+}
